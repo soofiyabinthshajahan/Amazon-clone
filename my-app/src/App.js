4a6e08d0568d21da414acc122e99f06154225860
Soofiya Binth Shajahan <soofiyashajahan@gmail.com>
2023-02-26 22:28:35 +0530

    fourth commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
-import Header from "./Head/Header";
 import React from "react";
+import Header from "./Head/Header";
+import Home from "./Home/Home";
 function App() {
   return (
     <div className="App">
-   <Header/>
+      <Header />
+      <Home />
     </div>
   );
 }
